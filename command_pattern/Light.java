1e7f0bd9762d02a864aa14d7c4c6b1da81558126
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2018-12-30 16:53:41 +0530

    Java Command Pattern

---
+++ command_pattern/Light.java
@@ -0,0 +1,18 @@
+package command_pattern;
+
+public class Light implements Command {
+	public void on() {
+		System.out.println("Light On");
+	}
+
+	public void off() {
+
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ command_pattern/RemoteControlTest.java
@@ -0,0 +1,13 @@
+package command_pattern;
+
+public class RemoteControlTest {
+
+	public static void main(String[] args) {
+		SimpleRemoteControl remote = new SimpleRemoteControl();
+		Light light = new Light();
+		LightOnCommand lightOn = new LightOnCommand(light);
+		remote.setCommand(lightOn);
+		remote.buttonWasPressed();
+	}
+
+}
